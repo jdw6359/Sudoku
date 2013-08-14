5b2d98d0bf70ebfd6d9384cc06d7b98cd06f12d5
Josh Woodward <jdw6359@rit.edu>
2013-08-13 22:36:36 -0400

    Finished the display board method of class "Board"

--- Board.py
+++ Board.py
@@ -3,7 +3,7 @@ Author: Josh Wooodward
 Date: 8/13/13
 
 Class is used to represent the current state of the board, and has methods
-that assist in using the board directly
+that assist in using the board directly 
 
 """
 class Board:
@@ -13,7 +13,18 @@ class Board:
         self.grid=[[0 for j in range(10) ] for i in range(10)]
    
     def displayBoard(self):
-        for i in self.grid:
-            print(i)
+        top=""
+        for i in range(0,9):
+            for j in range(0,9):
+                top+=str(self.grid[i][j])
+                top+=" "
+                if j==2 or j==5:
+                    top+="| "
+            top+="\n"
+            if i==2 or i==5:
+                top+="---------------------\n"
+        print(top)
+            
+
             
 
