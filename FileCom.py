a043c8a557b9e429dad5a234856823fce95302dc
Josh Woodward <jdw6359@rit.edu>
2013-08-15 21:22:12 -0400

    Added a FileCommunication object to read in the configuration files and create the initial board object

---
+++ FileCom.py
@@ -0,0 +1,36 @@
+"""
+Author: Josh Woodward
+Date: 8/15/13
+
+Class that will be used for file I/O
+"""
+from Board import * 
+
+class FileCom:
+
+    def __init__(self):
+        pass
+
+    """
+    will configure and return a board object
+    based on file_path
+    """
+    def read(self, config_file):
+        config=open("ConfigFiles/"+config_file)
+        board=Board()
+        row=0
+        col=0
+        for line in config:
+            for item in line:
+                if item != ".":
+                    board.setValue(row,col,item)
+                    
+                col+=1
+                    
+            row+=1
+            col=0
+        board.displayBoard()
+    
+            
+        
+        
